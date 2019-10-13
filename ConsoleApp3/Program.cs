27a17dd9d31cba1a42014cfb187804516373cb20
Ercan-home <ercanerdogan@gmail.com>
2019-10-14 02:56:50 +0300

    Advanced LINQ examples

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numList = new int[] { 1, 2, 3, 4, 5, 3, 3, 2, 5 };
+
+            //var query = (from item in numList
+            //             group item by item into g
+            //             orderby g.Count() descending
+            //             select new { Item = g.Key, Count = g.Count() });
+
+            
+
+            int searchKey = 2;
+
+
+
+            var query = (from item in numList
+                         group item by item into g
+                         orderby g.Count() descending
+                         select new { Key = g.Key, Value = g.Count() }).ToDictionary(pair => pair.Key, pair => pair.Value); ;
+
+          
+            int num = query[searchKey];
+
+            Console.Write(num);
+            Console.Read();
+        }
+    }
+}
